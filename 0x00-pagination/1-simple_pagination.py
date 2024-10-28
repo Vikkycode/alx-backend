b2a9256a0767acc4ccfc33c3d54491824d1d5899
victor oricha <vicky.robilson5528@gmail.com>
2024-10-28 16:34:21 +0100

    update 1 simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -44,11 +44,10 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        pass
         """
         get page
-
         """
+        pass
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
